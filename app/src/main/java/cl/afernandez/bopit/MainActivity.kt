acd84a1b1f5909ff5083ab988aa3e48da67fba01
Adolfo Fernandez <fantasmarobatortas5@gmail.com>
2023-09-16 01:17:42 -0300

    Crear el ACtivity de configuracion y hacerlo funcional, se cambia el color del fondo de la app al cambiar de activity

--- app/src/main/java/cl/afernandez/bopit/MainActivity.kt
+++ app/src/main/java/cl/afernandez/bopit/MainActivity.kt
@@ -1,20 +1,42 @@
 package cl.afernandez.bopit
 
 import android.content.Intent
+import android.content.SharedPreferences
+import android.graphics.Color
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.style.BackgroundColorSpan
+import android.util.Log
 import android.widget.Button
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.toColor
+import androidx.core.graphics.toColorInt
+import androidx.preference.PreferenceManager
 
 
 class MainActivity : AppCompatActivity() {
+    val tagLog ="error"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         val ButtonOkAbout = findViewById<Button>(R.id.aboutbutton)
         val ButtonOkSetting = findViewById<Button>(R.id.settingbutton)
 
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        val value = sharedPreferences.getString("Theme_color", "1")
+        val fondo = findViewById<ConstraintLayout>(R.id.Main_activity)
+
+        if(value.equals("1"))
+        {
+            fondo.background = ContextCompat.getDrawable(this, R.drawable.gradient1)
+        }else if (value.equals("2"))
+        {
+            fondo.background = ContextCompat.getDrawable(this, R.drawable.gradient2)
+        }
 
         ButtonOkAbout.setOnClickListener{
             val intentAbout = Intent(this, AboutActivity::class.java)
@@ -22,11 +44,25 @@ class MainActivity : AppCompatActivity() {
         }
 
         ButtonOkSetting.setOnClickListener{
-            val intentsetting = Intent(this, SettingActivity::class.java)
-            startActivity(intentsetting)
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
         }
 
+    }
 
+    override fun onStart() {
+        super.onStart()
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        val value = sharedPreferences.getString("Theme_color", "1")
+        val fondo = findViewById<ConstraintLayout>(R.id.Main_activity)
+
+        if(value.equals("1"))
+        {
+            fondo.background = ContextCompat.getDrawable(this, R.drawable.gradient1)
+        }else if (value.equals("2"))
+        {
+            fondo.background = ContextCompat.getDrawable(this, R.drawable.gradient2)
+        }
     }
 
 
--- app/src/main/java/cl/afernandez/bopit/SplashActivity.kt
+++ app/src/main/java/cl/afernandez/bopit/SplashActivity.kt
@@ -5,17 +5,33 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import androidx.preference.PreferenceManager
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        val value = sharedPreferences.getString("Theme_color", "1")
+
+        val fondoSplash = findViewById<ConstraintLayout>(R.id.Splash_activity)
+        if(value.equals("1"))
+        {
+            fondoSplash.background = ContextCompat.getDrawable(this, R.drawable.gradient1)
+        }else if (value.equals("2"))
+        {
+            fondoSplash.background = ContextCompat.getDrawable(this, R.drawable.gradient2)
+        }
+
         val handler = Handler(Looper.getMainLooper())
         handler.postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, 5000)
+        }, 3000)
     }
+
 }
