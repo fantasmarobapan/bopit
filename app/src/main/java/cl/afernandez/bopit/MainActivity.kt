d85d9d85068efa5be08bff5000f1d31935c982ba
Adolfo Fernandez <fantasmarobatortas5@gmail.com>
2023-10-26 01:46:52 -0300

    Primera Version, olvide copiar el xml string y el del sonido

--- app/src/main/java/cl/afernandez/bopit/MainActivity.kt
+++ app/src/main/java/cl/afernandez/bopit/MainActivity.kt
@@ -1,42 +1,21 @@
 package cl.afernandez.bopit
 
 import android.content.Intent
-import android.content.SharedPreferences
-import android.graphics.Color
-import android.graphics.drawable.Drawable
+import android.media.MediaPlayer
+import android.media.PlaybackParams
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.style.BackgroundColorSpan
-import android.util.Log
 import android.widget.Button
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.constraintlayout.widget.ConstraintSet
-import androidx.core.content.ContextCompat
-import androidx.core.graphics.toColor
-import androidx.core.graphics.toColorInt
-import androidx.preference.PreferenceManager
 
 
 class MainActivity : AppCompatActivity() {
-    val tagLog ="error"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val ButtonOkAbout = findViewById<Button>(R.id.aboutbutton)
         val ButtonOkSetting = findViewById<Button>(R.id.settingbutton)
-
-        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
-        val value = sharedPreferences.getString("Theme_color", "1")
-        val fondo = findViewById<ConstraintLayout>(R.id.Main_activity)
-
-        if(value.equals("1"))
-        {
-            fondo.background = ContextCompat.getDrawable(this, R.drawable.gradient1)
-        }else if (value.equals("2"))
-        {
-            fondo.background = ContextCompat.getDrawable(this, R.drawable.gradient2)
-        }
+        val ButtonTestMedia = findViewById<Button>(R.id.testmediabutton)
 
         ButtonOkAbout.setOnClickListener{
             val intentAbout = Intent(this, AboutActivity::class.java)
@@ -44,26 +23,15 @@ class MainActivity : AppCompatActivity() {
         }
 
         ButtonOkSetting.setOnClickListener{
-            val intent = Intent(this, SettingsActivity::class.java)
-            startActivity(intent)
+            val intentsetting = Intent(this, SettingsActivity::class.java)
+            startActivity(intentsetting)
+        }
+
+        ButtonTestMedia.setOnClickListener {
+            val intentTestMedia = Intent(this, SoundTestActivity::class.java)
+            startActivity(intentTestMedia)
         }
 
     }
-
-    override fun onStart() {
-        super.onStart()
-        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
-        val value = sharedPreferences.getString("Theme_color", "1")
-        val fondo = findViewById<ConstraintLayout>(R.id.Main_activity)
-
-        if(value.equals("1"))
-        {
-            fondo.background = ContextCompat.getDrawable(this, R.drawable.gradient1)
-        }else if (value.equals("2"))
-        {
-            fondo.background = ContextCompat.getDrawable(this, R.drawable.gradient2)
-        }
-    }
-
 
 }
---
+++ app/src/main/java/cl/afernandez/bopit/SoundTestActivity.kt
@@ -0,0 +1,64 @@
+package cl.afernandez.bopit
+
+import android.media.MediaPlayer
+import android.media.PlaybackParams
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class SoundTestActivity : AppCompatActivity() {
+    private lateinit var mediaPlayerWin: MediaPlayer
+    private lateinit var mediaPlayerLose: MediaPlayer
+    private lateinit var mediaPlayerMusic: MediaPlayer
+    private var playbackParams: PlaybackParams? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sound_test)
+
+        mediaPlayerWin = MediaPlayer.create(this, R.raw.winsound)
+        mediaPlayerLose = MediaPlayer.create(this, R.raw.errorsound)
+        mediaPlayerMusic = MediaPlayer.create(this, R.raw.backgroundmusic)
+        playbackParams = mediaPlayerMusic.playbackParams
+
+        val winButton = findViewById<Button>(R.id.winbutton)
+        val musicButton = findViewById<Button>(R.id.musicbutton)
+        val loseButton = findViewById<Button>(R.id.losebutton)
+        val speedButton = findViewById<Button>(R.id.speedbutton)
+        val slowButton = findViewById<Button>(R.id.slowbutton)
+
+        winButton.setOnClickListener{
+            mediaPlayerWin.start()
+        }
+
+        musicButton.setOnClickListener{
+            mediaPlayerMusic.isLooping = true
+            mediaPlayerMusic.start()
+        }
+
+        loseButton.setOnClickListener{
+            mediaPlayerLose.start()
+        }
+
+        speedButton.setOnClickListener{
+            playbackParams?.setSpeed(playbackParams!!.speed + 0.5f)
+            mediaPlayerMusic.playbackParams = playbackParams!!
+        }
+
+        slowButton.setOnClickListener{
+            playbackParams?.setSpeed(playbackParams!!.speed - 0.5f)
+            mediaPlayerMusic.playbackParams = playbackParams!!
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (mediaPlayerMusic.isPlaying){
+            mediaPlayerMusic.stop()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayerMusic.release()
+    }
+}
